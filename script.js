5c38079f839105192ed5ca51b59b1a4313fc3adf
FoodXJagan <foodxjagan01@gmail.com>
2026-06-20 09:07:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,68 +32,7 @@ window.addEventListener("load", () => {
 
 // Set your launch date here
 
-const launchDate =
-new Date("2026-12-31 23:59:59").getTime();
 
-function updateCountdown(){
-
-    const now =
-    new Date().getTime();
-
-    const distance =
-    launchDate - now;
-
-    if(distance < 0){
-
-        document.getElementById("timer")
-        .innerHTML = "LIVE NOW 🚀";
-
-        return;
-    }
-
-    const days =
-    Math.floor(
-        distance /
-        (1000 * 60 * 60 * 24)
-    );
-
-    const hours =
-    Math.floor(
-        (distance %
-        (1000 * 60 * 60 * 24))
-        /
-        (1000 * 60 * 60)
-    );
-
-    const minutes =
-    Math.floor(
-        (distance %
-        (1000 * 60 * 60))
-        /
-        (1000 * 60)
-    );
-
-    const seconds =
-    Math.floor(
-        (distance %
-        (1000 * 60))
-        /
-        1000
-    );
-
-    document.getElementById("timer")
-    .innerHTML =
-
-    `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-}
-
-updateCountdown();
-
-setInterval(
-    updateCountdown,
-    1000
-);
 
 
 // =========================
